9dc5f97b1936d788f496e59af0bdf03bf6d38966
taebong98 <thk98k@gmail.com>
2024-01-10 18:30:38 +0900

    Initial Commit

---
+++ src/main/java/com/taebong/szs/SzsApplication.java
@@ -0,0 +1,13 @@
+package com.taebong.szs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taebong/szs/SzsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taebong.szs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
