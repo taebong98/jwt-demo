6d530e02c63d4b5ecde9ab7c7dc522fda542394b
taebong98 <thk98k@gmail.com>
2024-01-11 16:48:35 +0900

    docs: 회원가입 API 문서 현행화

--- src/main/java/com/taebong/szs/controller/UserController.java
+++ src/main/java/com/taebong/szs/controller/UserController.java
@@ -3,6 +3,8 @@ package com.taebong.szs.controller;
 import com.taebong.szs.controller.dto.UserSignupDto;
 import com.taebong.szs.domain.UserService;
 import com.taebong.szs.domain.vo.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Tag(name = "사용자", description = "사용자 관련 API")
 @RestController
 @RequestMapping("/szs")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "회원가입")
     @PostMapping("/signup")
     public void postUser(@RequestBody @Valid UserSignupDto userSignupDto) {
         User user = userSignupDto.toUser();
--- src/main/java/com/taebong/szs/controller/dto/UserSignupDto.java
+++ src/main/java/com/taebong/szs/controller/dto/UserSignupDto.java
@@ -1,6 +1,7 @@
 package com.taebong.szs.controller.dto;
 
 import com.taebong.szs.domain.vo.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,16 +14,20 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 public class UserSignupDto {
     @NotBlank(message = "사용자 아이디는 필수 입력 항목입니다.")
+    @Schema(description = "아이디", defaultValue = "hong12")
     private String userId;
 
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    @Schema(description = "비밀번호", defaultValue = "123456")
     private String password;
 
     @NotBlank(message = "이름은 필수 입력 항목입니다.")
+    @Schema(description = "이름", defaultValue = "홍길동")
     private String name;
 
     @Pattern(regexp = "^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$")
     @NotBlank(message = "주민등록번호는 필수 입력 항목입니다.")
+    @Schema(description = "주민등록번호", defaultValue = "860824-1655068")
     private String regNo;
 
     public User toUser() {
