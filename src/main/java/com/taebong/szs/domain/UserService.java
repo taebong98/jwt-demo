b17d9fc9aebfe3160119cd20758a060d5332baf2
taebong98 <thk98k@gmail.com>
2024-01-11 13:08:07 +0900

    feat: 비밀번호, 주민등록번호는 암호화 시켜 저장해야한다.

--- src/main/java/com/taebong/szs/domain/UserService.java
+++ src/main/java/com/taebong/szs/domain/UserService.java
@@ -6,6 +6,7 @@ import com.taebong.szs.domain.vo.AllowedUsers;
 import com.taebong.szs.domain.vo.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ import java.util.Map;
 public class UserService {
     private final UserRepository userRepository;
     private final AllowedUsers allowedUsers;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void signup(User user) {
@@ -26,12 +28,33 @@ public class UserService {
             throw new ForbiddenException("회원가입 할 수 없는 사용자");
         }
 
-        userRepository.save(user);
+        User encodedUser = getEncodedUser(user);
+        userRepository.save(encodedUser);
+    }
+
+    private User getEncodedUser(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        String encodedRegNo = passwordEncoder.encode(user.getRegNo());
+        checkEncoded(user.getPassword(), encodedPassword);
+        checkEncoded(user.getRegNo(), encodedRegNo);
+
+        return User.builder()
+                .userId(user.getUserId())
+                .name(user.getName())
+                .password(encodedPassword)
+                .regNo(encodedRegNo)
+                .build();
     }
 
     private boolean isValidUser(String name, String regNo) {
         log.info("isValidUser().");
         Map<String, String> allowedUserMap = allowedUsers.getAllowedUserMap();
         return allowedUserMap.containsKey(regNo) && allowedUserMap.get(regNo).equals(name);
+    }
+
+    private void checkEncoded(String str, String encode) {
+        if (str.equals(encode)) {
+            throw new RuntimeException("암호화 수행되지 않음");
+        }
     }
 }
