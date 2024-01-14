5d8ad5aeb029161295c89fc36ac62b64cbbb3de8
taebong98 <thk98k@gmail.com>
2024-01-15 00:25:38 +0900

    feat: swagger, HttpHeaders 헤더 설정

--- src/main/java/com/taebong/szs/common/config/swagger/SwaggerConfig.java
+++ src/main/java/com/taebong/szs/common/config/swagger/SwaggerConfig.java
@@ -3,16 +3,30 @@ package com.taebong.szs.common.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("Authorization")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Authorization");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("Authorization", bearerAuth))
+                .addSecurityItem(addSecurityItem)
                 .info(getInfo());
     }
 
@@ -28,7 +42,7 @@ public class SwaggerConfig {
     private Info getInfo() {
         return new Info()
                 .version("v1.0.0")
-                .title("삼쩜삼 개발 과제 테스트 API 문서")
+                .title("SZS 개발 과제 테스트 API 문서")
                 .description("SZS");
     }
 }
